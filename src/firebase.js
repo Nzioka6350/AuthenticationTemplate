5d007bf597eb020ba12d5893e2a96df4bc855293
Nzioka6350 <jamesnziokamutua1@gmail.com>
2025-03-24 15:37:57 +0300

    Firebase methods for authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,6 +11,9 @@ const firebaseConfig = {
   appId: "1:655735147335:web:18a2793686b2ec27fe9a62"
 };
 
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
